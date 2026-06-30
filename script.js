c2d451df3694f73c6d8aaebdc11eeee135946fa1
youssief alaa el_din mohamed <23025086youssief@gmail.com>
2026-06-30 23:56:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(function(){
+
+document.getElementById("splash").style.display="none";
+
+document.getElementById("menu").style.display="block";
+
+document.body.style.overflow="auto";
+
+},2000);
